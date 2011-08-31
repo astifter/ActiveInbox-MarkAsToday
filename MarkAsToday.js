c390fcb85f0701ab96650c7db850821ace57151d
Andreas Neustifter <andreas.neustifter@a-trust.at>
2011-08-31 17:37:25 +0200

    Initial script.

---
+++ MarkAsToday.js
@@ -0,0 +1,41 @@
+var TODAY_LABEL = "S/Today";
+var OVERDUE_LABEL = "S/Overdue";
+
+function setup() {
+  // Create the labels well need
+  GmailApp.createLabel(OVERDUE_LABEL);
+  GmailApp.createLabel(TODAY_LABEL);
+}
+
+function markToday()
+{
+  var d = new Date();
+
+  var daylabel = "label:d-d-" + d.getDate();
+  var monthlabel = "label:d-m-" + (d.getMonth()+1);
+  var search = daylabel + " " + monthlabel;
+  
+  todaylabel = GmailApp.getUserLabelByName(TODAY_LABEL);
+  overduelabel = GmailApp.getUserLabelByName(OVERDUE_LABEL);
+  
+  page = null;
+  // get threads in "pages" of PAGE_SIZE at a time
+  while(!page || page.length == PAGE_SIZE) {
+    page = todaylabel.getThreads(0, PAGE_SIZE);
+    if (page.length > 0) {
+      // move the threads from "today" into "overdues" label
+      overduelabel.addToThreads(page);
+      todaylabel.removeFromThreads(page);
+    }  
+  }
+
+  page = null;
+  // get threads in "pages" of PAGE_SIZE at a time
+  while(!page || page.length == PAGE_SIZE) {
+    page = GmailApp.search(search, 0, PAGE_SIZE);
+    if (page.length > 0) {
+      // mark the threads from "today" search with "today" label
+      todaylabel.addToThreads(page);
+    }  
+  }
+}
